96a9a8b4df6196a99b116b391a47f5f0346b9995
MIP <32762839+mip4@users.noreply.github.com>
2017-10-13 04:40:09 -0400

    Add files via upload

---
+++ gaydiagnosis.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 13 03:27:21 2017
+
+@author: mip
+"""
+
+x = int(input('to find out if u r gay plz enter an integer 1-100: '))
+if x<101:
+    print('')
+    print('ur gay')
+else:
+    print('')
+    print('plz learn to follow instructions and try again')
